7aabfca11ff6e3c696885a67de43c1d10d9a3d01
Cameron Ostrom (RIT Student) <cxo1192@ad.rit.edu>
2019-10-16 14:50:04 -0400

    started mongo db demo

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,27 +1,54 @@
 // pull in our models. This will automatically load the index.js from that folder
 const models = require('../models');
 
+const Cat = models.Cat.CatModel;
+
 // default fake data so that we have something to work with until we make a real Cat
 const defaultData = {
   name: 'unknown',
   bedsOwned: 0,
 };
 
-let lastAdded;
+let lastAdded = new Cat(defaultData);
 
 const hostIndex = (req, res) => {
-
+  res.render('index', {
+    title: 'Home',
+    pageName: 'Home Page',
+    currentName: lastAdded.name,
+  });
 };
 
 const readAllCats = (req, res, callback) => {
-
+  Cat.find(callback);
 };
 
 const readCat = (req, res) => {
- 
+  const name1 = req.query.name;
+  
+  const callback = (err, doc) =>{
+    if(err){
+      return res.json({err});
+    }
+
+    return res.json(doc);
+  };
+
+  Cat.findByName(name1,callback);
 };
 
 const hostPage1 = (req, res) => {
+
+  const callback = (err, docs) =>{
+    if(err){
+      return res.json({err})
+    }
+    
+
+    return res.render('page1', {title: 'Cats', cats: docs,});
+  };
+
+  readAllCats(req,res,callback);
 
 };
 
@@ -34,14 +61,36 @@ const hostPage3 = (req, res) => {
 };
 
 const getName = (req, res) => {
-
+  res.json({name: lastAdded.name});
 };
 
 const setName = (req, res) => {
   if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
     return res.status(400).json({ error: 'firstname,lastname and beds are all required' });
   }
+
+
+  const name = `${req.body.firstname} ${req.body.lastname}`;
+
+  const catData = {
+    name,
+    bedsOwned: req.body.beds,
+  };
+
+  const newCat = new Cat(catData);
+
+  const savePromise = newCat.save();
   
+  savePromise.then(() =>{
+    lastAdded = newCat;
+
+    res.json({
+      name: lastAdded.name,
+      beds: lastAdded.bedsOwned,
+    });
+  });
+
+  savePromise.catch((err) => res.json({err}));
 };
 
 const searchName = (req, res) => {
